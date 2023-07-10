fd7c03dabb716ce6f5533904e8930daeb47180a4
desardi131 <desardi131@gmail.com>
2023-07-10 13:19:28 +0200

    changed the name of a variable in 01_01

--- 01_desafios_texto/01_01_palindromo_test.py
+++ 01_desafios_texto/01_01_palindromo_test.py
@@ -1,8 +1,8 @@
 def palindromo(text):
 
     text_lower = text.lower()
-    text_min = text_lower.replace(" ", "")
-    return text_min == text_min[::-1]
+    text_nospace = text_lower.replace(" ", "")
+    return text_nospace == text_min[::-1]
 
 
 frase_1 = "Dabale arroz a la zorra el abad"
